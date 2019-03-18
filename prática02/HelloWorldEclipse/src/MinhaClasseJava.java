3c31567da5aa9a5c6bfb47934aee3336a2177483
GUILHERME  KEESE TABACOW <17.00666-0@maua.br>
2019-03-18 11:43:42 -0300

    warning: LF will be replaced by CRLF in prática02/.metadata/.log. The file will have its original line endings in your working directory. warning: LF will be replaced by CRLF in prática02/.metadata/.plugins/org.eclipse.epp.logging.aeri.ui/server-config.j

---
+++ prática02/HelloWorldEclipse/src/MinhaClasseJava.java
@@ -0,0 +1,12 @@
+
+public class MinhaClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 25;
+		String nome = "Everson";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+	}
+
+}
